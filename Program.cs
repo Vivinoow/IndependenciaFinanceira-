b9b00c99ca3efdb5dfdce2f6005496e1dbc22d28
Viviane <vpereiradasilva55@gmail.com>
2022-09-17 09:40:07 -0300

    Atividade

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿decimal GastoMensais, ValorAInvestir;
+double dividendYield;
+
+Console.WriteLine("--- Vivendo de dividendos ---\n");
+
+Console.Write("Gastos mensais (em R$)...: ");
+GastoMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%)...:");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+ValorAInvestir = GastoMensais / Convert.ToDecimal(dividendYield / 100);
+
+Console.WriteLine($"\nPara receber {GastoMensais:C} por mês em rendimentos de {dividendYield:N1} % voce precisa ter investido { ValorAInvestir :C}.");
